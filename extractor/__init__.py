9cbf7bb3e902fab27c13e30ad69ebc349365e583
Erik Bergman <erikbergman@Eriks-MacBook-Air.local>
2026-02-05 12:15:44 +0100

    Add loading bar for loading Spacy

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,12 +1,14 @@
 from .cleaner import extract_text
 from .frequency import top_words
-from .tokenizer import lemma_groups, tokenize
+from .tokenizer import lemma_groups, preload_spacy, spacy_cached, tokenize
 from .utils import load_config
 
 __all__ = [
     "extract_text",
     "tokenize",
     "lemma_groups",
+    "preload_spacy",
+    "spacy_cached",
     "top_words",
     "load_config",
 ]
